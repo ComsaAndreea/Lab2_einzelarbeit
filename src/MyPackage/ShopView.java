85331ce63b49f014bf0e6f3c23f05c9840dd4a01
ComsaAndreea <52425269+ComsaAndreea@users.noreply.github.com>
2022-10-10 15:44:50 +0300

    final commit

--- src/MyPackage/ShopView.java
+++ src/MyPackage/ShopView.java
@@ -1,5 +1,7 @@
 package MyPackage;
 
+import java.util.Scanner;
+
 public class ShopView {
     public void printShopDetails(String ShopName, String ShopCategory, int ShopId, int ClientNumber){
         System.out.println("Shop Details: ");
@@ -8,4 +10,17 @@ public class ShopView {
         System.out.println("Shop ID: " + ShopId);
         System.out.println("Client Number: " + ClientNumber);
     }
+
+    public Shop createShop(){
+        Shop shop = new Shop();
+        Scanner in = new Scanner(System.in);
+        System.out.println("New Shop:");
+        System.out.println("Name: ");
+        System.out.println("Id: ");
+        System.out.println("Category: ");
+        System.out.println("Client Number: ");
+        return shop;
+        //mai trebuie sa fac citirea si sa adaug in main functionalitatea
+    }
+
 }
